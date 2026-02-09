adccec6ddcfe974b5af3af5f13d281848a94a565
Snip <185667147+snipwalter@users.noreply.github.com>
2026-02-09 03:09:47 -0500

    B: multiple messages

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+def main():
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    print(args.user_prompt)
 
-if api_key is None:
-    raise RuntimeError("API Key not found")
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-client = genai.Client(api_key=api_key)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
 
-response = client.models.generate_content( model='gemini-2.5-flash', contents="hi, reply with minimal words.")
-if response.usage_metadata is None:
-    raise RuntimeError("An error was occured during the api call")
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if api_key is None:
+        raise RuntimeError("API Key not found")
+
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
+    if response.usage_metadata is None:
+        raise RuntimeError("An error was occured during the api call")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+main()
